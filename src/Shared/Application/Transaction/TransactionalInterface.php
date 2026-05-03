5a78efcb9c2b7d13946f71a378669b2c4a1c22a0
Oleksandr Kopyl <alexsander11115@gmail.com>
2026-05-03 18:52:52 +0300

    feature/14-implement-registeraccount-http-api-endpoint
    Add `TransactionalInterface` to define transaction boundary handling (issue #14)

---
+++ src/Shared/Application/Transaction/TransactionalInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\Transaction;
+
+interface TransactionalInterface
+{
+    /**
+     * @template T
+     * @param callable(): T $operation
+     * @return T
+     */
+    public function transactional(callable $operation): mixed;
+}
