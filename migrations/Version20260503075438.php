e1403b4bf91a81978e153f6ba2a2f05f4e67f189
Oleksandr Kopyl <alexsander11115@gmail.com>
2026-05-03 10:59:56 +0300

    feature/9-implement-account-persistence-for-identityaccess
    Add migration for `accounts` table in IdentityAccess persistence (issue #9)

---
+++ migrations/Version20260503075438.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260503075438 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE accounts (id VARCHAR(36) NOT NULL, email VARCHAR(255) NOT NULL, password_hash VARCHAR(255) NOT NULL, name VARCHAR(100) NOT NULL, status VARCHAR(255) NOT NULL, is_system_admin BOOLEAN DEFAULT false NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, approved_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, rejected_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, disabled_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, version INT DEFAULT 1 NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('ALTER TABLE accounts ADD CONSTRAINT uniq_accounts_email UNIQUE (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE accounts');
+    }
+}
+
